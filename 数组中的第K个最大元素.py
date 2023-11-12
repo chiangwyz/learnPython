0157a3c1d22c9570a63058b8a2c1d49631d7c3a3
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-11-12 16:22:10 +0800

    Create 数组中的第K个最大元素.py

---
+++ 数组中的第K个最大元素.py
@@ -0,0 +1,34 @@
+"""
+通过随机选择一个基准数（pivot），然后将数组分为三部分：大于、等于和小于基准数的元素。这种方法在处理包含重复元素的数组时特别有效，因为它将等于 pivot 的所有元素归为一类，这样可以减少递归时的数组大小。
+在这个实现中：
+1. 如果第 k 大的元素在 big 数组中，递归地在 big 中寻找。
+2. 如果第 k 大的元素不在 big 中，但在 small 数组中，根据 k 和 small 数组的大小递归地在 small 中寻找。
+3. 如果第 k 大的元素不在 big 和 small 中，它必然在 equal 中，直接返回 pivot。
+"""
+import random
+
+class Solution:
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        def quick_select(nums, k):
+            # 随机选择基准数
+            pivot = random.choice(nums)
+            big, equal, small = [], [], []
+            # 将大于、小于、等于 pivot 的元素划分至 big, small, equal 中
+            for num in nums:
+                if num > pivot:
+                    big.append(num)
+                elif num < pivot:
+                    small.append(num)
+                else:
+                    equal.append(num)
+
+            if k <= len(big):
+                # 第 k 大元素在 big 中，递归划分
+                return quick_select(big, k)
+            if len(nums) - len(small) < k:
+                # 第 k 大元素在 small 中，递归划分
+                return quick_select(small, k - len(nums) + len(small))
+            # 第 k 大元素在 equal 中，直接返回 pivot
+            return pivot
+        
+        return quick_select(nums, k)
