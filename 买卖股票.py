22bbdf4c18c62ffc9262414eaf8483b0dbc679e6
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-11-14 23:28:00 +0800

    Create 买卖股票.py

---
+++ 买卖股票.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        # 如果数组为空或只有一个价格数据，无法进行交易
+        if len(prices) < 2:
+            return 0
+
+        # 初始化最小价格为第一天的价格
+        min_price = prices[0]
+        # 初始化最大利润为0
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                if price - min_price > max_profit:
+                    max_profit = price - min_price
+
+        return max_profit
+
+
