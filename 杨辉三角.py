286f012724a6ea96d9946aee8101b5b08dfa1e1c
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-11-14 23:01:55 +0800

    Create 杨辉三角.py

---
+++ 杨辉三角.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        if numRows == 0:
+            return []
+
+        # 初始化杨辉三角
+        triangle = [[1]*(i+1) for i in range(numRows)]
+
+        # 从第三行开始填充杨辉三角
+        for i in range(2, numRows):
+            for j in range(1, i):
+                triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+
+        return triangle
+
+
+
