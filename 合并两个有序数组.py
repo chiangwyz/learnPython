5519e685055f14d5b287d4846e08b7f7a4bb86f9
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-11-18 23:15:34 +0800

    Update comments

--- 合并两个有序数组.py
+++ 合并两个有序数组.py
@@ -1,9 +1,12 @@
+"""
+这种解法非常巧妙，所以记录下来，从后往前合并，而且考虑了m=0或者n=0的情况
+"""
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-            # 从后向前合并，先填充nums1的后半部分
+        # 从后向前合并，先填充nums1的后半部分
         while m > 0 and n > 0:
             if nums1[m - 1] > nums2[n - 1]:
                 nums1[m + n - 1] = nums1[m - 1]
