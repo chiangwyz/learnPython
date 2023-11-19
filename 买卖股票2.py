891a7b73330594c398aab705a133009d4211984d
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-11-19 23:01:16 +0800

    Create 买卖股票2.py

---
+++ 买卖股票2.py
@@ -0,0 +1,12 @@
+"""
+这里的关键是，由于题目允许我们在同一天卖出后立即再次买入，我们可以连续捕捉每次价格上涨的机会。
+无论价格变动如何，只要有价格上涨，我们都可以通过买入和卖出来累积利润。
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        return max_profit
