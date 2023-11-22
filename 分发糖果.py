901bcf46025ea7a824d889dc302d0c743a223e57
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-11-22 09:41:42 +0800

    Create 分发糖果.py

---
+++ 分发糖果.py
@@ -0,0 +1,29 @@
+"""
+n 个孩子站成一排。给你一个整数数组 ratings 表示每个孩子的评分。
+你需要按照以下要求，给这些孩子分发糖果：
+每个孩子至少分配到 1 个糖果。
+相邻两个孩子评分更高的孩子会获得更多的糖果。
+请你给每个孩子分发糖果，计算并返回需要准备的 最少糖果数目 。
+"""
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        left = [0] * n
+        for i in range(n):
+            if i > 0 and ratings[i] > ratings[i - 1]:
+                left[i] = left[i - 1] + 1
+            else:
+                left[i] = 1
+        
+        right = ret = 0
+        for i in range(n - 1, -1, -1):
+            if i < n - 1 and ratings[i] > ratings[i + 1]:
+                right += 1
+            else:
+                right = 1
+            ret += max(left[i], right)
+        
+        return ret
+
+
