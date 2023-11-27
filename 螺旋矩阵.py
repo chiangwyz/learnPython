2445d78f229f8863d58e7eb65b070fb963e7b23f
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-11-27 10:59:24 +0800

    Update 螺旋矩阵.py

--- 螺旋矩阵.py
+++ 螺旋矩阵.py
@@ -1,1 +1,18 @@
+class Solution:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        res = []
+        while matrix:
+            # 添加第一行并移除
+            res += matrix.pop(0)
+            # 逆时针旋转矩阵
+            matrix = list(zip(*matrix))[::-1]
+        return res
+
+
+
+
+
+
+
+
 
