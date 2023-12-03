cfb180e95bc087804013733553c54025cc156b9e
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-03 21:09:25 +0800

    Create 二叉树的右视图.py

---
+++ 二叉树的右视图.py
@@ -0,0 +1,24 @@
+"""
+给定一个二叉树的 根节点 root，想象自己站在它的右侧，
+按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+
+示例 1:
+输入: [1,2,3,null,5,null,4]
+输出: [1,3,4]
+
+示例 2:
+输入: [1,null,3]
+输出: [1,3]
+
+示例 3:
+输入: []
+输出: []
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+"""
+
