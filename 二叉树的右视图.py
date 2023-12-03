4dcae78ce17d2f5b1a7d37a7a1d475f4dbe3a8c6
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-03 22:05:21 +0800

    Update 二叉树的右视图.py

--- 二叉树的右视图.py
+++ 二叉树的右视图.py
@@ -20,5 +20,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+核心思路：
+使用层序遍历（广度优先搜索）来遍历二叉树的每一层。
+在每一层中，记录最后一个节点的值，因为这是从右侧看到的节点。
 """
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []  # 如果树为空，则返回空列表
+
+        queue = collections.deque([root])  # 使用双端队列初始化，开始时只有根节点
+        rightside = []  # 存储每一层最右侧节点的值
+
+        while queue:
+            level_length = len(queue)  # 获取当前层的节点数量
+            for i in range(level_length):  # 遍历当前层的每个节点
+                node = queue.popleft()  # 弹出一个节点
+                if i == level_length - 1:  # 如果是当前层的最后一个节点
+                    rightside.append(node.val)  # 将其值添加到结果列表
+                if node.left:  # 如果节点有左子节点
+                    queue.append(node.left)  # 将左子节点添加到队列
+                if node.right:  # 如果节点有右子节点
+                    queue.append(node.right)  # 将右子节点添加到队列
+
+        return rightside  # 返回最终结果
+
+
+
+
+
+
+
+
 
