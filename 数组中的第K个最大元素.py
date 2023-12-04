d57415e201cffbed86afef98f72c7a8106a8c66f
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-04 23:21:35 +0800

    Update 数组中的第K个最大元素.py

--- 数组中的第K个最大元素.py
+++ 数组中的第K个最大元素.py
@@ -1,5 +1,7 @@
 """
-通过随机选择一个基准数（pivot），然后将数组分为三部分：大于、等于和小于基准数的元素。这种方法在处理包含重复元素的数组时特别有效，因为它将等于 pivot 的所有元素归为一类，这样可以减少递归时的数组大小。
+通过随机选择一个基准数（pivot），然后将数组分为三部分：大于、等于和小于基准数的元素。
+这种方法在处理包含重复元素的数组时特别有效，因为它将等于 pivot 的所有元素归为一类，
+这样可以减少递归时的数组大小。
 在这个实现中：
 1. 如果第 k 大的元素在 big 数组中，递归地在 big 中寻找。
 2. 如果第 k 大的元素不在 big 中，但在 small 数组中，根据 k 和 small 数组的大小递归地在 small 中寻找。
@@ -24,12 +26,14 @@ class Solution:
                 else:
                     equal.append(num)
 
-            if k <= len(big):
+            if len(big) >= k:
                 # 第 k 大元素在 big 中，递归划分
                 return quick_select(big, k)
-            if len(nums) - len(small) < k:
+                
+            if len(big) + len(equal) < k:
                 # 第 k 大元素在 small 中，递归划分
-                return quick_select(small, k - len(nums) + len(small))
+                return quickSelect(small, k - len(big) - len(equal))
+                
             # 第 k 大元素在 equal 中，直接返回 pivot
             return pivot
         
