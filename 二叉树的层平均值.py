d04f94b52cb0a8ed2413416df3ec0a132bbe842e
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-05 09:48:46 +0800

    Update 二叉树的层平均值.py comments

--- 二叉树的层平均值.py
+++ 二叉树的层平均值.py
@@ -13,6 +13,13 @@
 4.返回存储平均值的列表。
 """
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
         if not root:
