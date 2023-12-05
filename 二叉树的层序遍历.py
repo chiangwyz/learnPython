3beeb8c8d6bf097c027d677b9c1a85c1c15d1f9a
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-05 10:57:15 +0800

    Update 二叉树的层序遍历.py

--- 二叉树的层序遍历.py
+++ 二叉树的层序遍历.py
@@ -1,1 +1,37 @@
+"""
+给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
 
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> list[list[int]]:
+        if not root:
+            return []
+        
+        result = []
+        queue = collections.deque([root])
+
+        while queue:
+            level_length = len(queue)
+
+            tmp = []
+            for _ in range(level_length):
+                node = queue.popleft()
+                tmp.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(tmp)
+
+        return result
