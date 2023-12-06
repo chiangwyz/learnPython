f46a2332d08289dda704783df33dff3848eee9c2
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-06 11:27:00 +0800

    Update 三角形最小路径和.py

--- 三角形最小路径和.py
+++ 三角形最小路径和.py
@@ -1,1 +1,36 @@
+"""
+定一个三角形 triangle ，找出自顶向下的最小路径和。
+每一步只能移动到下一行中相邻的结点上。
+相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。
+
+ 
+
+示例 1：
+输入：triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+输出：11
+解释：如下面简图所示：
+   2
+  3 4
+ 6 5 7
+4 1 8 3
+自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。
+
+示例 2：
+输入：triangle = [[-10]]
+输出：-10
+"""
+
+class Solution:
+    def minimumTotal(self, triangle: list[list[int]]) -> int:
+        n = len(triangle)
+        # 从三角形的倒数第二层开始向上计算
+        for i in range(n - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                # 更新当前位置的最小路径和
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+        return triangle[0][0]
+
+
+
 
