7a1080437a5b03bbb9227d7c88a9a0cc8baa2a0f
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-06 12:30:38 +0800

    Update 最小路径和.py

--- 最小路径和.py
+++ 最小路径和.py
@@ -1,6 +1,5 @@
 """
 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
-
 说明：每次只能向下或者向右移动一步。
 
 示例 1：
@@ -13,6 +12,32 @@
 输出：12
 """
 
+class Solution:
+    def minPathSum(self, grid: list[list[int]]) -> int:
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        dp = [[0 for _ in range(cols)] for _ in range(rows)]
+
+        dp[0][0] = grid[0][0]
+
+        # 初始化第一列
+        for i in range(1, rows):
+            dp[i][0] = dp[i-1][0] + grid[i][0]
+
+        # 初始化第一行
+        for j in range(1, cols):
+            dp[0][j] = dp[0][j-1] + grid[0][j]
+
+        # 填充剩下的单元格
+        for i in range(1, rows):
+            for j in range(1, cols):
+                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+
+        return dp[rows-1][cols-1]
+
+      
 
 
 
