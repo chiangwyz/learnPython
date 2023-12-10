2ff1f8942a1868a80870502d2e754e9770c00bdf
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-10 22:06:49 +0800

    Update 有效的数独.py

--- 有效的数独.py
+++ 有效的数独.py
@@ -21,32 +21,33 @@ j // 3 是 7 // 3，等于 2，表示它在第三组列。
 """
 
 # Python 函数来验证数独是否有效
-def isValidSudoku(board):
-    # 为数独的每一行、每一列和每个3x3宫初始化字典
-    rows = [{} for _ in range(9)]
-    columns = [{} for _ in range(9)]
-    boxes = [{} for _ in range(9)]
-
-    # 遍历数独的每一个单元格
-    for i in range(9):
-        for j in range(9):
-            num = board[i][j]
-            # 只检查已填的单元格
-            if num != '.':
-                num = int(num)
-                # 计算当前单元格所在的3x3宫索引
-                box_index = (i // 3) * 3 + j // 3
-
-                # 将当前单元格的数字添加到对应的行、列和宫字典中
-                rows[i][num] = rows[i].get(num, 0) + 1
-                columns[j][num] = columns[j].get(num, 0) + 1
-                boxes[box_index][num] = boxes[box_index].get(num, 0) + 1
-
-                # 检查当前数字在对应的行、列和宫是否出现了多次
-                if rows[i][num] > 1 or columns[j][num] > 1 or boxes[box_index][num] > 1:
-                    return False
-
-    return True
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        # 为数独的每一行、每一列和每个3x3宫初始化字典
+        rows = [{} for _ in range(9)]
+        columns = [{} for _ in range(9)]
+        boxes = [{} for _ in range(9)]
+    
+        # 遍历数独的每一个单元格
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                # 只检查已填的单元格
+                if num != '.':
+                    num = int(num)
+                    # 计算当前单元格所在的3x3宫索引
+                    box_index = (i // 3) * 3 + j // 3
+    
+                    # 将当前单元格的数字添加到对应的行、列和宫字典中
+                    rows[i][num] = rows[i].get(num, 0) + 1
+                    columns[j][num] = columns[j].get(num, 0) + 1
+                    boxes[box_index][num] = boxes[box_index].get(num, 0) + 1
+    
+                    # 检查当前数字在对应的行、列和宫是否出现了多次
+                    if rows[i][num] > 1 or columns[j][num] > 1 or boxes[box_index][num] > 1:
+                        return False
+    
+        return True
 
 # 示例数独棋盘
 board_example = [
