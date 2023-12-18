8d6fe277a1cf60e1d0f0f68abad4d12f98a23515
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-18 17:19:48 +0800

    Update 赎金信.py

--- 赎金信.py
+++ 赎金信.py
@@ -3,10 +3,8 @@
 如果可以，返回 true ；否则返回 false 。
 magazine 中的每个字符只能在 ransomNote 中使用一次。
 
-
 解题思路：
-通过比较 magazine 字符串中字符的数量是否满足 ransomNote 字符串中相应字符的需求，
-来决定 ransomNote 能否由 magazine 构成。
+通过比较 magazine 字符串中字符的数量是否满足 ransomNote 字符串中相应字符的需求，来决定 ransomNote 能否由 magazine 构成。
 这个过程中，magazine 中的每个字符计数都会被逐一减去 ransomNote 中相应字符的使用，
 如果 magazine 中的字符数量足够，最终将返回 True，表示可以构成；
 如果任何时候字符数量不足，将返回 False，表示无法构成。
