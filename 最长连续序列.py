5d2b35dbad7f1caa9cfb60981266222ae99f36dc
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-18 23:08:21 +0800

    Update 最长连续序列.py

--- 最长连续序列.py
+++ 最长连续序列.py
@@ -5,13 +5,14 @@
 这样保证了整个算法的时间复杂度为 O(n)，因为每个数字最多只被访问两次：一次是在将 nums 转换为集合时，一次是在找到序列开始时。
 """
 class Solution:
-    def longestConsecutive(self, nums: list[int]) -> int:
+    def longestConsecutive(self, nums: List[int]) -> int:
         # 创建一个包含所有元素的集合，用于O(1)的查找
         nums_set = set(nums)
         longest_streak = 0
         current_streak = 0
 
         for num in nums_set:
+            # num - 1 这个判断是解决这个问题的关键
             if num - 1 not in nums_set:
                 current_num = num
                 current_streak = 1
