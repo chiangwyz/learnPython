4c7113a7385a1a355a870c363154e624a3611489
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-18 22:47:22 +0800

    Update 存在重复元素II.py

--- 存在重复元素II.py
+++ 存在重复元素II.py
@@ -1,13 +1,16 @@
 class Solution:
-    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
-        index_map = {}  # 存储元素及其最近出现的索引
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        # 存储元素及其最近出现的索引
+        index_map = {}  
 
         for i, num in enumerate(nums):
             if num in index_map and i - index_map[num] <= k:
-                return True  # 发现重复元素，且它们的索引差不大于 k
-            index_map[num] = i  # 更新元素的最新索引
-
-        return False  # 遍历完数组后没有找到满足条件的元素对
+                # 发现重复元素，且它们的索引差不大于 k
+                return True  
+            # 更新元素的最新索引
+            index_map[num] = i  
+        # 遍历完数组后没有找到满足条件的元素对
+        return False  
 
 # 示例测试
 sol = Solution()
