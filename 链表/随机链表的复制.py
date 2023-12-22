88f983359f941b57d931f7229c631fd591fd7726
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-22 15:16:05 +0800

    Update 随机链表的复制.py

--- 链表/随机链表的复制.py
+++ 链表/随机链表的复制.py
@@ -1,1 +1,47 @@
+"""
+要解决这个问题，我们可以遵循以下步骤：
+
+1. 迭代原始链表：
+对于原始链表的每个节点，创建一个新节点，其值与原始节点相同。将新节点插入到原始节点和其下一个节点之间。
+
+2. 更新随机指针：
+迭代修改后的链表，设置每个新节点的随机指针。如果原始节点 A 的随机指针指向节点 B，那么复制的节点 A' 的随机指针应指向复制的节点 B'。
+
+3. 拆分链表：
+最后，将修改后的链表拆分为原始链表和复制链表。
+
+这个过程中的关键在于保持原始链表不变，同时创建新链表的副本。
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        # 第一步：复制每个节点，并将复制节点放置在原节点后面
+        current = head
+        while current:
+            new_node = Node(current.val, current.next)
+            current.next = new_node
+            current = new_node.next
+
+        # 第二步：设置新节点的随机指针
+        current = head
+        while current:
+            if current.random:
+                current.next.random = current.random.next
+            current = current.next.next
+
+        # 第三步：拆分链表
+        old_list = head
+        new_list = head.next
+        new_head = head.next
+        while old_list:
+            old_list.next = old_list.next.next
+            new_list.next = new_list.next.next if new_list.next else None
+            old_list = old_list.next
+            new_list = new_list.next
+
+        return new_head
+
 
