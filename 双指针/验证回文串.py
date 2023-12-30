c665fb38db06faded9e9869d4325a61543f431c6
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2023-12-30 21:52:09 +0800

    Update 验证回文串.py

--- 双指针/验证回文串.py
+++ 双指针/验证回文串.py
@@ -2,12 +2,12 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
          # 初始化一个空字符串用来存储筛选后的字符
         good_str = str()
+        # 逐词遍历
         for char in s:
             # 如果字符是字母或数字
             if char.isalnum():
                 # 转换为小写并追加到good_str字符串
                 good_str += char.lower()
-
 
         # 获取过滤后字符串的长度
         left = 0
@@ -21,12 +21,3 @@ class Solution:
             right -= 1
 
         return True
-
-
-
-
-
-
-
-
-
