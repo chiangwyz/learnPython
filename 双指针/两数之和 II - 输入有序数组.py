8182d874fe9ad55b4cb191bdfdd232f05428f8de
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2024-01-01 22:43:01 +0800

    Update and rename 两数之和 II - 输入有序数组.py to 双指针/两数之和 II - 输入有序数组.py

--- 双指针/两数之和 II - 输入有序数组.py
+++ 双指针/两数之和 II - 输入有序数组.py
@@ -1,21 +1,30 @@
 """
 使用了双指针策略来找出两个数的和等于给定目标数的元素下标。由于数组是按非递减顺序排列的，这种方法可以有效地在常量空间和线性时间内找到解。
+
+注意，下标是从1开始的。
 """
 
 class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        left, right = 0, len(numbers) - 1  # 使用双指针，分别指向数组的开始和结束
-        
-        while left < right:  # 当左指针小于右指针时循环
+        # 使用双指针，分别指向数组的开始和结束
+        left = 0
+        right = len(numbers) - 1  
+
+        # 当左指针小于右指针时循环
+        while left < right:  
             current_sum = numbers[left] + numbers[right]
-            if current_sum == target:  # 如果两数之和等于目标值，返回它们的下标
+            # 如果两数之和等于目标值，返回它们的下标
+            if current_sum == target:  
                 return [left + 1, right + 1]
-            elif current_sum < target:  # 如果两数之和小于目标值，移动左指针
+            # 如果两数之和小于目标值，移动左指针
+            elif current_sum < target:  
                 left += 1
-            else:  # 如果两数之和大于目标值，移动右指针
+            # 如果两数之和大于目标值，移动右指针
+            else:  
                 right -= 1
-        
-        return []  # 如果没有找到，返回空数组
+
+        # 如果没有找到，返回空数组
+        return []  
 
 # 示例测试
 sol = Solution()
