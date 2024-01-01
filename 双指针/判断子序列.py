437c1e3a9f5892e986296613d948c260b96adc32
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2024-01-01 22:35:19 +0800

    Update and rename 判断子序列.py to 双指针/判断子序列.py

--- 双指针/判断子序列.py
+++ 双指针/判断子序列.py
@@ -1,9 +1,11 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         # 双指针初始化为两个字符串的起始位置
-        pointer_s, pointer_t = 0, 0
+        pointer_s = 0
+        pointer_t = 0
         # 获取两个字符串的长度
-        len_s, len_t = len(s), len(t)
+        len_s = len(s)
+        len_t = len(t)
 
         # 当两个指针都未达到各自字符串的末尾时，进行循环
         while pointer_s < len_s and pointer_t < len_t:
