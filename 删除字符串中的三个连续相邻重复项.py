df7ff209860586a17c75292f7835e1dc23d04744
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2024-01-05 22:54:42 +0800

    Update 删除字符串中的三个连续相邻重复项.py

--- 删除字符串中的三个连续相邻重复项.py
+++ 删除字符串中的三个连续相邻重复项.py
@@ -1,7 +1,7 @@
 class Solution:
     def removeTripleDuplicates(self, s: str) -> str:
         """
-        在字符串S中执行删除操作，删除连续三个相同的字符。
+        在字符串S中执行删除操作，删除连续三个或以上相同的字符。
         重复执行此操作，直到不能再删除为止。
         返回处理后的字符串。
         :param S: str - 由小写字母组成的字符串
@@ -9,19 +9,24 @@ class Solution:
         """
         stack = []
         for char in s:
-            # 检查栈顶是否有两个连续且相同的字符
+            # 当栈中至少有两个字符时，检查是否需要删除连续的字符
             if len(stack) >= 2 and stack[-1] == char and stack[-2] == char:
-                # 如果找到了三个连续的字符，则弹出栈顶的两个字符
-                stack.pop()
-                stack.pop()
+                # 如果当前字符与栈顶两个字符相同，则继续检查栈中后续字符是否相同，直到找到不同的字符
+                while stack and stack[-1] == char:
+                    stack.pop()
             else:
-                # 否则，将当前字符推入栈中
+                # 如果不符合删除条件，将当前字符推入栈中
                 stack.append(char)
         # 将栈中剩余的字符连接起来，形成最终的字符串
         return ''.join(stack)
 
 # 测试函数
-input_string = "abbbaaca"
 solution = Solution()
+input_string = "abbbaaca"
 result = solution.removeTripleDuplicates(input_string)
-print(result)
+print(result) # ca
+
+input_string2 = "aabbbacbbddfaffffg"
+result2 = solution.removeTripleDuplicates(input_string2)
+print(result2) # ca
+
