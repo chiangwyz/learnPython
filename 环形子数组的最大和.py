f89266118aebbf950d3f2425cb305054c8683f9d
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2024-01-05 22:26:13 +0800

    Update 环形子数组的最大和.py

--- 环形子数组的最大和.py
+++ 环形子数组的最大和.py
@@ -44,7 +44,8 @@ class Solution:
             """
             Kadane算法，用于计算最大子数组和
             """
-            max_ending_here = max_so_far = nums[0]
+            max_ending_here = nums[0]
+            max_so_far = nums[0]
             for x in nums[1:]:
                 max_ending_here = max(x, max_ending_here + x)
                 max_so_far = max(max_so_far, max_ending_here)
