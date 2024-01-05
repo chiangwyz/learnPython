0d2f034353fdb09c2392b99f07fb1dfeba219e47
chiangwyz <35353716+chiangwyz@users.noreply.github.com>
2024-01-05 14:30:55 +0800

    Update 环形子数组的最大和.py

--- 环形子数组的最大和.py
+++ 环形子数组的最大和.py
@@ -10,7 +10,7 @@
 
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        def kadane(nums):
+        def kadane(nums: List[int]):
             """Kadane算法，用于计算最大子数组和"""
             max_ending_here = max_so_far = nums[0]
             for x in nums[1:]:
