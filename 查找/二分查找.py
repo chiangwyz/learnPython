32abf23c5977cd906428c0921d1ec06912c6a592
chiangwyz <1119027571@qq.com>
2024-01-12 15:28:21 +0800

    feature: add binary search

---
+++ 查找/二分查找.py
@@ -0,0 +1,22 @@
+"""
+给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
+写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+"""
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        if not nums:
+            return -1
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                return mid
+
+        return -1
