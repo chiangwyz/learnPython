6495d44214a0d46c8c9a7280a04a62d6611e68d5
chiangwyz <1119027571@qq.com>
2024-01-29 15:53:55 +0800

    feature: add k sorted arrays

---
+++ 堆/合并k个有序数组(堆法).py
@@ -0,0 +1,38 @@
+"""
+合并k个有序数组为一个有序数组。
+"""
+
+# 方法一
+
+import heapq
+
+
+class Solution:
+    def merge_k_sorted_arrays(self, arrays):
+        # 创建一个最小堆
+        min_heap = []
+        # 初始化堆，将每个数组的第一个元素及其索引和数组索引放入堆中
+        for i, array in enumerate(arrays):
+            if array:
+                heapq.heappush(min_heap, (array[0], i, 0))
+
+        result = []
+        # 当堆不为空时，继续处理
+        while min_heap:
+            # 弹出最小元素
+            val, arr_idx, ele_idx = heapq.heappop(min_heap)
+            result.append(val)
+            # 如果该数组还有元素，将下一个元素加入堆中
+            if ele_idx + 1 < len(arrays[arr_idx]):
+                heapq.heappush(min_heap, (arrays[arr_idx][ele_idx + 1], arr_idx, ele_idx + 1))
+
+        return result
+
+
+# test
+solution = Solution()
+
+# 示例输入
+arrays = [[1, 4, 5], [1, 3, 4], [2, 6]]
+
+print(solution.merge_k_sorted_arrays(arrays))
---
+++ 堆/合并k个有序数组(数组法).py
@@ -0,0 +1,37 @@
+"""
+合并k个有序数组，使用数组法。
+"""
+
+
+class Solution:
+    def merge_two_sorted_arrays(self, arr1, arr2):
+        # 合并两个有序数组
+        result = []
+        i, j = 0, 0
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] < arr2[j]:
+                result.append(arr1[i])
+                i += 1
+            else:
+                result.append(arr2[j])
+                j += 1
+        # 添加剩余的元素
+        result.extend(arr1[i:])
+        result.extend(arr2[j:])
+        return result
+
+    def merge_k_sorted_arrays_sequential(self, arrays):
+        if not arrays:
+            return []
+        merged_array = arrays[0]
+        for i in range(1, len(arrays)):
+            merged_array = self.merge_two_sorted_arrays(merged_array, arrays[i])
+        return merged_array
+
+
+# test
+solution = Solution()
+arrays = [[1, 4, 5], [1, 3, 4], [2, 6]]
+
+# 再次使用相同的示例输入
+print(solution.merge_k_sorted_arrays_sequential(arrays))
