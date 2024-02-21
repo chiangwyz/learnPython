7a331d9c12442df2ba73973f96282c618e412198
chiangwyz <1119027571@qq.com>
2024-02-21 10:12:07 +0800

    feature: add unittest

--- 数组(字符串)/移除元素.py
+++ 数组(字符串)/移除元素.py
@@ -12,8 +12,10 @@
 输出：5, nums = [0,1,3,0,4]
 解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
 """
+
+
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
+    def removeElement(self, nums: list[int], val: int) -> int:
         # 初始化一个新的index
         nex_index = 0
 
@@ -22,6 +24,47 @@ class Solution:
             if nums[i] != val:
                 nums[nex_index] = nums[i]
                 nex_index += 1
-        
+
         # 返回的nex_index即为数组长度
         return nex_index
+
+
+# 单元测试
+import unittest
+
+
+class TestRemoveElement(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_empty_list(self):
+        nums = []
+        val = 3
+        length = self.solution.removeElement(nums, val)
+        self.assertEqual(length, 0)
+        self.assertEqual(nums[:length], [])
+
+    def test_list_without_val(self):
+        nums = [1, 2, 3, 4, 5]
+        val = 6
+        length = self.solution.removeElement(nums, val)
+        self.assertEqual(length, 5)
+        self.assertEqual(nums[:length], [1, 2, 3, 4, 5])
+
+    def test_list_with_val(self):
+        nums = [3, 2, 2, 3]
+        val = 3
+        length = self.solution.removeElement(nums, val)
+        self.assertEqual(length, 2)
+        self.assertEqual(sorted(nums[:length]), [2, 2])
+
+    def test_list_all_val(self):
+        nums = [4, 4, 4, 4]
+        val = 4
+        length = self.solution.removeElement(nums, val)
+        self.assertEqual(length, 0)
+        self.assertEqual(nums[:length], [])
+
+
+if __name__ == "__main__":
+    unittest.main()
