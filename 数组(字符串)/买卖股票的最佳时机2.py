dadc34189f721188f295e2dbbc4d7a27a4a483e9
chiangwyz <1119027571@qq.com>
2024-02-23 10:10:08 +0800

    feature: update code

--- 数组(字符串)/买卖股票的最佳时机2.py
+++ 数组(字符串)/买卖股票的最佳时机2.py
@@ -1,10 +1,16 @@
 """
+给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+
+在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。你也可以先购买，然后在 同一天 出售。
+
+返回 你能获得的 最大 利润 。
+
 由于题目允许我们在同一天卖出后立即再次买入，我们可以连续捕捉每次价格上涨的机会。
 无论价格变动如何，只要有价格上涨，我们都可以通过买入和卖出来累积利润。
 """
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
         max_profit = 0
         for i in range(1, len(prices)):
             if prices[i] > prices[i - 1]:
