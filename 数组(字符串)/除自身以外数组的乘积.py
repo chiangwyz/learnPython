c524ea8133db053003b2800f8e942b29e5a8a3ea
chiangwyz <1119027571@qq.com>
2024-02-26 11:49:48 +0800

    feature: add commit

--- 数组(字符串)/除自身以外数组的乘积.py
+++ 数组(字符串)/除自身以外数组的乘积.py
@@ -6,18 +6,25 @@
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         # 数组长度
-        length = len(nums)  
+        length = len(nums)
+
+        if length == 0:
+            return []
 
         # 初始化左乘积、右乘积和结果数组
-        left, right, answer = [0]*length, [0]*length, [0]*length
+        left = [0]*length
+        right = [0] * length
+        answer = [0] * length
 
         # 计算左乘积
-        left[0] = 1  # 第一个元素左边没有其他元素，所以乘积为1
+        # 第一个元素左边没有其他元素，所以乘积为1
+        left[0] = 1
         for i in range(1, length):
             left[i] = left[i-1] * nums[i-1]
 
         # 计算右乘积
-        right[length-1] = 1  # 最后一个元素右边没有其他元素，所以乘积为1
+        # 最后一个元素右边没有其他元素，所以乘积为1
+        right[length-1] = 1
         for i in range(length-2, -1, -1):
             right[i] = right[i+1] * nums[i+1]
 
@@ -28,5 +35,26 @@ class Solution:
         return answer
 
 
-# 示例测试
-print(productExceptSelf([1,2,3,4]))  # 输出 [24,12,8,6]
+import unittest
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_example(self):
+        self.assertEqual(self.solution.productExceptSelf([1,2,3,4]), [24,12,8,6])
+
+    def test_zero(self):
+        self.assertEqual(self.solution.productExceptSelf([0,1]), [1,0])
+
+    def test_negative(self):
+        self.assertEqual(self.solution.productExceptSelf([-1,2,-3,4]), [-24,12,-8,6])
+
+    def test_single_element(self):
+        self.assertEqual(self.solution.productExceptSelf([42]), [1])
+
+    def test_empty(self):
+        self.assertEqual(self.solution.productExceptSelf([]), [])
+
+
+if __name__ == "__main__":
+    unittest.main()
