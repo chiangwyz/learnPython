ef327dcd2fbc8ff8d75bbcdd320cf1ba286f48a0
chiangwyz <1119027571@qq.com>
2024-06-09 23:57:19 +0800

    add torch

---
+++ torch/反向传播.py
@@ -0,0 +1,16 @@
+import torch
+
+# 创建一个张量，并启用自动求导
+x = torch.tensor([1.0, 2.0, 3.0], requires_grad=True)
+
+# 清除之前的梯度值
+x.grad = torch.zeros_like(x)  # 或者 x.grad.zero_()，但第一次需要初始化
+
+# 计算损失
+y = x.sum()
+
+# 执行反向传播
+y.backward()
+
+# 查看梯度
+print(x.grad)
