1307c8e2492078f357d2293f7b45f73b31922f42
chiangwyz <1119027571@qq.com>
2024-06-12 22:51:26 +0800

    add linear regression concise

---
+++ torch_learning/002linear_regression_concise.py
@@ -0,0 +1,53 @@
+import torch
+import random
+# nn是神经网络的缩写
+from torch import nn
+import numpy as np
+from torch.utils import data
+
+
+def synthetic_data(w, b, num_examples):  #@save
+    """生成y=Xw+b+噪声"""
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1, 1))
+
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = synthetic_data(true_w, true_b, 1000)
+
+
+def load_array(data_arrays, batch_size, is_train=True):  #@save
+    """构造一个PyTorch数据迭代器"""
+    dataset = data.TensorDataset(*data_arrays)
+    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+
+batch_size = 10
+data_iter = load_array((features, labels), batch_size)
+
+
+net = nn.Sequential(nn.Linear(2, 1))
+net[0].weight.data.normal_(0, 0.01)
+net[0].bias.data.fill_(0)
+
+loss = nn.MSELoss()
+
+trainer = torch.optim.SGD(net.parameters(), lr=0.03)
+
+num_epochs = 3
+for epoch in range(num_epochs):
+    for X, y in data_iter:  # 调用data_iter函数
+        l = loss(net(X) ,y)
+        trainer.zero_grad()
+        l.backward()
+        trainer.step()
+    l = loss(net(features), labels)
+    print(f'epoch {epoch + 1}, loss {l:f}')
+
+w = net[0].weight.data
+print('w的估计误差：', true_w - w.reshape(true_w.shape))
+b = net[0].bias.data
+print('b的估计误差：', true_b - b)
